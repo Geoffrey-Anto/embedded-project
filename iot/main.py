da4bfd669e7874e91bf2aa45900f6190e2028be4
Geoffrey-Anto <geoffreyanto.ignatiuse2021@vitstudent.ac.in>
2024-04-26 00:10:02 +0530

    updated: rfid code

--- iot/main.py
+++ iot/main.py
@@ -1,39 +1,33 @@
 import RPi.GPIO as GPIO
-import MFRC522
-import signal
-from gpiozero import Servo
+from mfrc522 import SimpleMFRC522
 from time import sleep
 
-continue_reading = True
-
-def end_read(signal,frame):
-    global continue_reading
-    print("Ctrl+C captured, ending read.")
-    continue_reading = False
-    GPIO.cleanup()
-
-signal.signal(signal.SIGINT, end_read)
-
-rfid = MFRC522.MFRC522()
+from gpiozero import Servo
 
 servo = Servo(25)
 
-def main():
-    while continue_reading:
-        (status,TagType) = rfid.MFRC522_Request(rfid.PICC_REQIDL)
-        if status == rfid.MI_OK:
-            print("Card detected")
-        
-        (status,uid) = rfid.MFRC522_Anticoll()
+led = 40
 
-        if status == rfid.MI_OK:
-            print ("Card read UID: %s,%s,%s,%s" % (uid[0], uid[1], uid[2], uid[3]))
-            servo.min()
-            sleep(0.5)
-            servo.mid()
-            sleep(0.5)
-            servo.max()
-            sleep(0.5)
+GPIO.setwarnings(False)    # Ignore warning for now
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(led, GPIO.OUT)
+GPIO.output(led, GPIO.LOW)
+reader = SimpleMFRC522()
 
+while True:
+    try:
 
-main()
+        id, text = reader.read()
+        print(id)
+        print(type(id))
+        print(text)
+        sleep(2)
+
+        if id == 872589727:
+            sleep(2)
+            GPIO.output(led, GPIO.HIGH)
+            sleep(1)
+        else:
+            GPIO.output(led, GPIO.LOW)
+    except:
+        GPIO.cleanup()
